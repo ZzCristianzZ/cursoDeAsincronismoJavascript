a93d4d365a44a17c67e26578653fffca6c19a8d9
ZzCristianzZ <crisfegomez@gmail.com>
2023-01-31 17:44:13 -0500

    Se agrega archivo de pruebas

---
+++ src/pruebas.js
@@ -0,0 +1,28 @@
+// const pepe = () => {
+//     console.log('Hola mundo');
+// };
+
+// pepe();
+
+// Arrow Funtion
+
+const sss = (x) => x*x;
+console.log(square(2)); // 4
+
+// Arrow Function autoEjecutable
+
+(()=>{console.log('soy una funcion de flecha anonima autoejecutable')})(); // soy una funcion de flecha anonima autoejecutable
+
+()=>{console.log('soy una funcion de flecha anonima autoejecutable')}
+
+
+function rSquare (x) {
+    return x*x;
+};
+
+const square = function (x){
+    return x*x;
+};
+
+
+const squareArrow = (x) => x*x;
