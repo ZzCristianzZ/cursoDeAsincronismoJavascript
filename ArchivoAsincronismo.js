b0c4858f61dd07b532579875d8dcd5e8a4f5af17
ZzCristianzZ <crifegomez@gmail.com>
2023-02-02 17:15:59 -0500

    Se Agrega la clase en la que vimos Fetch, el uso de peticiones con dos ejemplos. Uno haciendo una peticion basica, y otro con las peticiones que hicimos con la API XMLHttpRequest.

--- ArchivoAsincronismo.js
+++ ArchivoAsincronismo.js
@@ -1,39 +1,39 @@
-const  XMLHttpRequest = require ('xmlhttprequest').XMLHttpRequest;
-const API = 'https//api.escuelajs.co/api/v1';
+// const  XMLHttpRequest = require ('xmlhttprequest').XMLHttpRequest;
+// const API = 'https//api.escuelajs.co/api/v1';
 
-function fetchData (urlApi, callback){
-    let xhhtp = new XMLHttpRequest();
+// function fetchData (urlApi, callback){
+//     let xhhtp = new XMLHttpRequest();
 
-    xhhtp.open ('GET', urlApi, true);
+//     xhhtp.open ('GET', urlApi, true);
 
-    xhhtp.onreadystatechange = function(event){
-        if (xhhtp.onreadystatechange === 4){
-            if (xhhtp.status === 200){
-                callback(null, JSON.parse(xhhtp.responseText))
-            }else{
-                const error = new Error('Error' + urlApi);
-                return callback(error, null);
-            }
-        }
-    }
-    xhhtp.send();
-}
+//     xhhtp.onreadystatechange = function(event){
+//         if (xhhtp.onreadystatechange === 4){
+//             if (xhhtp.status === 200){
+//                 callback(null, JSON.parse(xhhtp.responseText))
+//             }else{
+//                 const error = new Error('Error' + urlApi);
+//                 return callback(error, null);
+//             }
+//         }
+//     }
+//     xhhtp.send();
+// }
 
-fetchData(`${API}/products`, function(error1,data1){
-    if (error1) return console.error(error1);
+// fetchData(`${API}/products`, function(error1,data1){
+//     if (error1) return console.error(error1);
 
-    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
-        if (error2) return console.error(error2);
+//     fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+//         if (error2) return console.error(error2);
 
-        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3){
-            if (error3) return console.error(error3);
+//         fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3){
+//             if (error3) return console.error(error3);
             
-            console.log(data1[0]);
-            console.log(data2.title);
-            console.log(data3.name);
-        });
-    });
-});
+//             console.log(data1[0]);
+//             console.log(data2.title);
+//             console.log(data3.name);
+//         });
+//     });
+// });
 
 // CALLBACK HELL
 // Se traduce en el uso de de varias llamadas o peticiones a la API con el condicional If
@@ -57,3 +57,84 @@ fetchData(`${API}/products`, function(error1,data1){
 //con solo .then se obtiene el resultado de la promesa de acuerdo a resolve o reject
 //con .catch podemos obtener más información de un futuro error que se presente
 //con .finally podemos imprimir un mensaje que indica que ya se ejecutó la promesa.
+
+
+//QUE ES FETCH?
+// Fetch : Es una API (Interfas de Programacion Grafica), Que proporciona una forma estandar y mas sencilla para realizar solicitudes HTTP por medio de promesas de ECMAScript 6. Sirve para asincrono como sincrono. Y Permite recuperar recursos en forma binaria(Imagenes, audios y videos).
+
+// INSTALACION DE FETCH
+// Ir a la terminal y escribir <npm i node-fetch>
+// Para compilar desde Visual Studio Code, debemos registrar el modulo en el archivo package.json. Se abre el archivo y se agrega al final "type" : "module". Importante agregar una coma a la llave que cierra las dependencies.
+
+
+// import fetch from 'node-fetch';
+// const API = 'https://api.escuelajs.co/api/v1';
+
+// function fetchData (urlApi){
+//     return fetch(urlApi);
+// };
+
+// fetchData (`${API}/products`)
+//     .then(response => response.json())
+//     .then(products => {
+//         console.log('products');
+//     })
+//     .then(() => {
+//         console.log('Hola');
+//     })
+//     .catch(error => console.log(error));
+
+import fetch from 'node-fetch';
+// Esta linea importa el metodo fetch desde el paquete node-fetch
+const API = 'https://api.escuelajs.co/api/v1';
+// Esta linea crea una constante llamada API que contiene la Url de la Api de la Escuela Js
+
+function fetchData (urlApi){
+    return fetch(urlApi);
+    // esta es una funcion que retorna la funcion Fetch con la urlApi. La funcion fetch permite hacer peticiones HTTP .Devuelve una promesa que se puede encadenar con los metodos then(), y catch para manejar errores
+};
+
+// fetchData (`${API}/products`)
+// // se hace llamado a la Funcion fetchData 
+//     .then(response => response.json())
+//     // coge el resultado de la funcion y la tranforma en un objeto .json
+//     .then(products => {
+//         console.log('products');
+//     // Este linea imprime el resultado, es decir el objeto, en consola.
+//     })
+//     .then(() => {
+//         console.log('Hola');
+//     })
+//     .catch(error => console.log(error));
+    // esta linea toma los errores que puedan ocurrir y los imprime en consola.
+
+// SEGUNDO EJEMPLO, HACER VARIOS LLAMADOS O PETICIONES Y TRAER  LO MISMO QUE HICIMOS CON LA API XMLHTTPRequest
+
+//Esta serie de líneas de código intenta realizar una solicitud a una API para obtener la información de productos y categorías.
+fetchData(`${API}/products`)
+//Primero se hace una llamada a la API con la dirección especificada por la constante API y se almacena el resultado en una variable.
+    .then(response => response.json()) //se hace la conversión a un objeto json
+    .then(products => {
+        console.log(products);
+        return fetchData(`${API}/products/${products[0].id}`) // solo se quiere mostrar el primer elemento de la primera solicitud
+        // Luego, se hace una solicitud con el identificador del primer elemento de la primera solicitud.
+        
+    })
+    .then(response => response.json()) //se vuelve traer la data
+    .then(product => {
+        console.log(product.title);
+        return fetchData(`${API}/categories/${product.category.id}`) //se quiere mostrar la categoria de un producto en particular// Se solicita la categoría de un producto en particular.
+        
+    })
+    .then(response => response.json())
+    .then(category => {
+        console.log(category.name);//Se imprime el nombre de la categoría 
+    })
+    .catch(err => console.log(err)) //detectar un error
+    .finally(() => console.log('Finally')); //es opcional para mostrar que se terminó la solicitud
+
+
+
+
+
+
