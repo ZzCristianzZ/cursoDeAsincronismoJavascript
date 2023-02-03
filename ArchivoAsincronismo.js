4a42285b9743f52aff7a58bd6c1b4a6a881cf32a
ZzCristianzZ <crifegomez@gmail.com>
2023-02-03 01:17:46 -0500

    se agregan la clase de funciones asincronicas que permiten la ejecucion del codigo sin ser bloqueantes con las promesas

--- ArchivoAsincronismo.js
+++ ArchivoAsincronismo.js
@@ -186,3 +186,39 @@ postData(`${API}/products`, data)
     .then(data => console.log(data));
 
 
+// ----------------------------------------------------------------------------------------
+// FUNCIONES ASINCRONAS
+// La declaracion de la Funcion Async, define una funcion asincrona que devuelve un objeto, lo cual permite a un programa correr una funcion sin congelar todo la compilacion. Se hace para simplificar el comportamiento sincrono de las promesas.La estructura de las funciones asicncronas es : 
+
+// La palabra async que va antes de la funcion que devuelve una promesa.
+// La palabra await que va dentro de las funciones async que espera hasta que la promesa se resuelve para continuar 
+
+// QUE ES EL IF TERNARIO 
+// sintaxis : condicion ? expresion1 : expresion2;
+//  EJEMPLO DE LAS FUNCIONES ASINCRONAS 
+
+const fnAsync = () =>{
+    return new Promise((resolve, reject) =>{
+        (true) //se usó operador ternario y se está forzando con true que se cumpla la condición
+            ? setTimeout(() => resolve('Async!!', 2000))
+            : reject(new Error('Error!')); //arroja "error" en caso de que la condición sea false
+    });
+}
+
+const anotherFn = async () => { //la palabra async es para el cuerpo de la función
+    //la palabra await estará dentro de la lógica a implementar
+    const something = await fnAsync(); //aquí nos está regresando una promesa
+    console.log(something); //se imprime mientras se espera
+    console.log('Hello!');
+}
+
+console.log('Before'); //al ser la primera orden con solo console.log, 'Before' se imprime primero
+anotherFn();//es el segundo en llamar, pero aún así no se imprimen los console de su lógica y tarda 2 s en ser ejecutada
+console.log('After'); //aparece justo después de 'Before' porque anotherFn() está esperando una promesa y aún así el programa no se detiene, sino que sigue y así tenemos 'After' de segundo al imprimir
+
+/*La salida al correr con Run Code queda:
+Before
+After
+Async!!
+Hello!
+*/
